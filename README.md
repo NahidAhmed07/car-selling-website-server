443add319bce37ac0b4379849cc17dc550baeaa6
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-11-13 16:51:41 +0600

    user order api updated

---
+++ README.md
@@ -0,0 +1,41 @@
+# ElectroCar-car-selling-Website
+
+## Live site link
+
+[live site Link](https://electro-car-shop.netlify.app/) : https://electro-car-shop.netlify.app/
+
+## Using Technology and Programming Language
+
+- React.js
+- Material-Ui
+
+* Node.js
+* Express.js
+* MongoDB As Database
+
+- React-Router-Dom
+- Context Api
+- Firebase Authentication system
+
+## Top Features
+
+- Car selling Website including admin panel.
+- Option for Admin to manage all product And Delete and Edit Product.
+- Option for Admin to manage all order Status And cancel Order.
+- Customer Review Systems.
+- Option for Admin to Add a New Product.
+- Implement the Firebase Authentication system.
+
+## uses of This Website
+
+- This Website help you easily Buy your favorite Car and Jeep.
+- You can Payment money wit payPal .
+- Buy your Favorite Car.
+- Help to you Know Our facilities
+- Our latest news and special Offer and Discount
+- 2 Year Free Car servicing facilities.
+- You may also take our 24/7 Emergency helpline service.
+
+## Nahid Ahmed (owner This Repo and This Website)
+
+- junior Web Developer at TEAM Rejected .
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongo db connection uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4v0cg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -18,10 +19,12 @@ const client = new MongoClient(uri, {
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+// initialize firebase Jwt Token verification
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+// Verify Jwt Token middleWare Function
 const verifyToken = async (req, res, next) => {
   if (req?.headers?.authorization?.startsWith("Bearer ")) {
     const IdToken = req?.headers?.authorization.split(" ")[1];
@@ -39,6 +42,7 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("car_shop");
+    // all database collection
     const usersCollection = database.collection("users");
     const productsCollection = database.collection("products");
     const ordersCollection = database.collection("orders");
@@ -46,26 +50,75 @@ async function run() {
     const topCarCollection = database.collection("topCar");
     const BlogCollection = database.collection("blog");
 
+    //############################# All Product Related API Start Here ######################################
     // get all product
     app.get("/product/all", async (req, res) => {
       const result = await productsCollection.find({}).toArray();
       res.send(result);
     });
-    // get all product
+    // get product for home page
+    app.get("/product/home", async (req, res) => {
+      const result = await productsCollection.find({}).limit(6).toArray();
+      res.send(result);
+    });
+
+    // get product for product page
+    app.get("/product/single/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // add a new product to database
+    app.post("/addProduct", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.json(result);
+    });
+
+    // delete product
+    app.delete("/product", async (req, res) => {
+      const id = req.query.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    //  update product
+    app.put("/product", async (req, res) => {
+      const id = req.query.id;
+      const newData = req.body;
+      const filter = { _id: ObjectId(id) };
+      delete newData._id;
+      const option = { upsert: true };
+      const updateDoc = { $set: newData };
+      const result = await productsCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
+      res.json(result);
+    });
+    //############################# All Product Related API END Here ######################################
+
+    //############################# All Order Related API STart Here ######################################
+
+    // get all order
     app.post("/orders", async (req, res) => {
       const order = req.body;
       const result = await ordersCollection.insertOne(order);
       res.send(result);
     });
 
-    // get myorder api
-
+    // get my order api
     app.get("/myOrder", verifyToken, async (req, res) => {
       const decodedEmail = req.decodedEmail;
       const email = req.query.email;
       if (decodedEmail) {
         const query = { email: email };
         const result = await ordersCollection.find(query).toArray();
+        // check user order is Empty ?
         if (result.length > 0) {
           res.send(result);
         } else {
@@ -104,27 +157,9 @@ async function run() {
       );
       res.json(result);
     });
+    //############################# All Order Related API END Here ######################################
 
-    // get product for home page
-    app.get("/product/home", async (req, res) => {
-      const result = await productsCollection.find({}).limit(6).toArray();
-      res.send(result);
-    });
-
-    // get product for product page
-    app.get("/product/single/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await productsCollection.findOne(query);
-      res.send(result);
-    });
-
-    // add a new product to database
-    app.post("/addProduct", async (req, res) => {
-      const product = req.body;
-      const result = await productsCollection.insertOne(product);
-      res.json(result);
-    });
+    //############################# All User Related API Start Here ######################################
     // save user information to database
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -154,6 +189,9 @@ async function run() {
       res.json(result);
     });
 
+    //############################# All User Related API END Here ######################################
+
+    //############################# All Admin Related API STArt Here ######################################
     // add a admin
     app.post("/addAdmin", verifyToken, async (req, res) => {
       const email = req.query.email;
@@ -186,30 +224,9 @@ async function run() {
       }
     });
 
-    // delete product
-    app.delete("/product", async (req, res) => {
-      const id = req.query.id;
-      const query = { _id: ObjectId(id) };
-      const result = await productsCollection.deleteOne(query);
-      res.json(result);
-    });
+    //############################# All Admin Related API end Here ######################################
 
-    //  update product
-    app.put("/product", async (req, res) => {
-      const id = req.query.id;
-      const newData = req.body;
-      const filter = { _id: ObjectId(id) };
-      delete newData._id;
-      const option = { upsert: true };
-      const updateDoc = { $set: newData };
-      const result = await productsCollection.updateOne(
-        filter,
-        updateDoc,
-        option
-      );
-      res.json(result);
-    });
-
+    //############################# Others All API Start Here ######################################
     // top car api
     app.get("/topCar", async (req, res) => {
       const result = await topCarCollection.find({}).toArray();
